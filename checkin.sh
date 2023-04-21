d79a3f9bdf5a629bb1302bc5f24efbfaf8fa23e9
spartan058 <hyxk1120302996@163.co>
2023-04-21 21:57:29 +0800

    message

--- checkin.sh
+++ checkin.sh
@@ -3,4 +3,4 @@ curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '
 echo '-----------------status-----------------'
 leftDays=`curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"' |awk -F '"' '{print $4}' | awk -F '.' '{print $1}'`
 echo '-----------------Notify-----------------'
-curl "https://sctapi.ftqq.com/SCT206874TDXkYKVRQ15az1U0fIiQukc0u.send?title=leftDays:360"
+curl -i -X GET -H 'Content-Type: text/xml; charset=UTF-8' https://sctapi.ftqq.com/SCT206874TDXkYKVRQ15az1U0fIiQukc0u.send?title=leftDays:360
